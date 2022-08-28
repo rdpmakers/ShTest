8ba64d22065f463eaa64d0a43edf3afec52544ea
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-08-28 09:30:35 +0700

    Create DebianXngrok.sh

---
+++ DebianXngrok.sh
@@ -0,0 +1,40 @@
+sudo apt-get update
+sudo apt-get -y install lxde
+
+adduser desktop
+usermod -aG sudo desktop
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3389 &>/dev/null &
+echo "===================================="
+echo "RDP Is bring started"
+echo "===================================="
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo ""
+echo ""
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+echo "Wait to finish bot and next open RDC to connect"
+echo "===================================="
+echo "===================================="
