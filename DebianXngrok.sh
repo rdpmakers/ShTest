0f9f4fb6880d3ed177733fed6b53e2c60340f994
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-08-28 09:35:34 +0700

    Update DebianXngrok.sh

--- DebianXngrok.sh
+++ DebianXngrok.sh
@@ -33,4 +33,4 @@ echo "username : desktop"
 echo "password : your password"
 echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-
+sleep 3600
